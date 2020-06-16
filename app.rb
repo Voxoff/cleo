4dafcf902debe24d51a003293e7fe3367af59068
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-06-16 13:46:41 +0100

    high level main file

---
+++ app.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative './lib/coin'
+require_relative './lib/item'
+require_relative './lib/vending_machine'
+require_relative './lib/view'
+
+change = { 1 => 2, 2 => 1, 5 => 5, 10 => 10, 20 => 3, 50 => 2, 100 => 10, 200 => 1 }
+items = { 'Nuka-Cola': 90, 'Sunset Sarsparilla': 200, 'Sierra Madre': 45, 'Bawls Guarana': 110 }
+
+machine = VendingMachine.new(items: items, change: change)
+machine.turn_on
